45efa60d6f510f030ee576361fc316c89e120e7c
Zamba Izo <ofuzak@gmail.com>
2024-11-21 20:51:49 -0500

    move deploy to scriptoshi

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,30 +1,48 @@
 #!/bin/bash
 set -e
+
+# Default values for environment variables
+SITE_DIR=${SITE_DIR:-"betriver.scriptoshi.com"}
+REPO_NAME=${REPO_NAME:-"betriver"}
+WWW_DIR="/var/www"
+
 echo "Deployment started ..."
-echo "# checkout main version of the app"
-cd /var/www/demo && git checkout main
+echo "Deploying to site directory: $SITE_DIR"
+
+echo "# Checkout main version of the app"
+cd $WWW_DIR/$REPO_NAME && git checkout main
+
 echo "# Install composer dependencies"
 composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+
 echo "# Install & Compile npm assets"
 npm i && npm run build
-echo "build complete!"
-echo "# Run fresh database migrations and seed"
+echo "Build complete!"
+
+echo "# Run fresh database migrations"
 php artisan migrate --force
-#php artisan db:seed --force
+
 echo "Swapping Folders"
-rm -rf /var/www/demo_betn_io_back
-mv /var/www/demo.betn.io /var/www/demo_betn_io_back
-mv /var/www/demo /var/www/demo.betn.io
-#copy over storage files
-rm -r /var/www/demo.betn.io/storage;
-[ -d "/var/www/demo_betn_io_back/storage" ] && mv /var/www/demo_betn_io_back/storage /var/www/demo.betn.io/storage
-#file pond upload dir
-mkdir -p /var/www/demo.betn.io/storage/app/public/uploads/
-rm -rf /var/www/demo.betn.io/storage/app/filepond || :
-echo "# Run post instalation setup"
-cd /var/www/demo.betn.io && php artisan clear-compiled
-cd /var/www/demo.betn.io && php artisan optimize
-cd /var/www/demo.betn.io && php artisan storage:link
-cd /var/www/demo.betn.io && sudo chgrp -R www-data storage bootstrap/cache
-cd /var/www/demo.betn.io && sudo chmod -R ug+rwx storage bootstrap/cache
-echo "Betn Deployment Finished"
+rm -rf ${WWW_DIR}/${REPO_NAME}_back
+mv ${WWW_DIR}/${SITE_DIR} ${WWW_DIR}/${REPO_NAME}_back
+mv ${WWW_DIR}/${REPO_NAME} ${WWW_DIR}/${SITE_DIR}
+
+echo "# Handling storage directory"
+rm -r ${WWW_DIR}/${SITE_DIR}/storage
+[ -d "${WWW_DIR}/${REPO_NAME}_back/storage" ] && \
+    mv ${WWW_DIR}/${REPO_NAME}_back/storage ${WWW_DIR}/${SITE_DIR}/storage
+
+# Create upload directory
+mkdir -p ${WWW_DIR}/${SITE_DIR}/storage/app/public/uploads/
+rm -rf ${WWW_DIR}/${SITE_DIR}/storage/app/filepond || :
+
+echo "# Run post installation setup"
+cd ${WWW_DIR}/${SITE_DIR} && {
+    php artisan clear-compiled
+    php artisan optimize
+    php artisan storage:link
+    sudo chgrp -R www-data storage bootstrap/cache
+    sudo chmod -R ug+rwx storage bootstrap/cache
+}
+
+echo "$SITE_DIR Deployment Finished"
