7ab686fbbab67207a6bfed358c68951262b0ab96
Zamba Izo <ofuzak@gmail.com>
2024-12-12 10:51:29 -0500

    Fix s3 uploads

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,11 +39,10 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
-
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
@@ -55,7 +54,16 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
-
+        'do' => [
+            'driver' => 's3',
+            'key' => env('DO_SPACES_KEY'),
+            'secret' => env('DO_SPACES_SECRET'),
+            'endpoint' => env('DO_SPACES_ENDPOINT'),
+            'region' => env('DO_SPACES_REGION'),
+            'bucket' => env('DO_SPACES_BUCKET'),
+            'cdn' => env('DO_SPACES_CDN'),
+            'url' => env('DO_SPACES_CDN'),
+        ],
     ],
 
     /*
